Fixed mysterious sampled softmax bug with cleaner variable scoping.

chatbot/bot_ops.py
@@ -19,7 +19,7 @@ def dynamic_sampled_softmax_loss(labels, logits, output_projection, vocab_size,
         Returns:
             loss as a scalar Tensor, computed as the mean over all batches and sequences.
     """
-    with tf.name_scope(name, "dynamic_sampled_softmax_loss", [labels, logits]):
+    with tf.name_scope(name, "dynamic_sampled_softmax_loss", [labels, logits, output_projection]):
         seq_len  = tf.shape(logits)[1]
         st_size  = tf.shape(logits)[2]
         time_major_outputs  = tf.reshape(logits, [seq_len, -1, st_size])
@@ -27,19 +27,19 @@ def dynamic_sampled_softmax_loss(labels, logits, output_projection, vocab_size,
         # Reshape is apparently faster (dynamic) than transpose.
         w_t = tf.reshape(output_projection[0], [vocab_size, -1])
         b = output_projection[1]
-        def proj_op(elem):
+        def sampled_loss(elem):
             logits, lab = elem
-            lab = tf.reshape(lab, [-1, 1])
             # TODO: Figure out how this accurately gets loss without requiring weights,
             # like sparse_softmax_cross_entropy requires.
             return tf.reduce_mean(
                 tf.nn.sampled_softmax_loss(
                     weights=w_t,
                     biases=b,
-                    labels=lab,
+                    labels=tf.expand_dims(lab, -1),
                     inputs=logits,
                     num_sampled=num_samples,
                     num_classes=vocab_size,
                     partition_strategy='div'))
-        batch_losses = tf.map_fn(proj_op, (time_major_outputs, time_major_labels), dtype=tf.float32)
-    return tf.reduce_mean(batch_losses)
+        batch_losses = tf.map_fn(sampled_loss, (time_major_outputs, time_major_labels), dtype=tf.float32)
+        loss = tf.reduce_mean(batch_losses)
+    return loss

chatbot/dynamic_models.py
@@ -138,7 +138,8 @@ class DynamicBot(Model):
                 if sampled_loss and 0 < self.num_samples < self.vocab_size:
                     self.loss = bot_ops.dynamic_sampled_softmax_loss(
                         target_labels, self.outputs[:, :-1, :],
-                        self.decoder.get_projection_tensors(), self.vocab_size)
+                        self.decoder.get_projection_tensors(), self.vocab_size,
+                        num_samples=self.num_samples)
                 else:
                     self.loss = tf.losses.sparse_softmax_cross_entropy(
                         labels=target_labels, logits=self.outputs[:, :-1, :],
@@ -158,11 +159,11 @@ class DynamicBot(Model):
                 correct_pred = tf.equal(tf.round(tf.argmax(proj[:, :-1, :], axis=2)),
                                         tf.round(tf.argmax(target_labels)))
                 accuracy = tf.reduce_mean(tf.cast(correct_pred, tf.float32))
-            # Creating a summar.scalar tells TF that we want to track the value for visualization.
-            tf.summary.scalar('accuracy', accuracy)
-            tf.summary.scalar('loss', self.loss),
-            tf.summary.scalar('learning_rate', self.learning_rate),
-            self.merged = tf.summary.merge_all()
+                # Creating a summar.scalar tells TF that we want to track the value for visualization.
+                tf.summary.scalar('accuracy', accuracy)
+                tf.summary.scalar('loss', self.loss),
+                tf.summary.scalar('learning_rate', self.learning_rate),
+                self.merged = tf.summary.merge_all()
 
         # Next, let superclass load param values from file (if not reset), otherwise
         # initialize newly created model.
@@ -239,7 +240,8 @@ class DynamicBot(Model):
 
                     # Toggle data switch and led the validation flow!
                     self.pipeline.toggle_active()
-                    summaries, eval_loss, _ = self.step(forward_only=True)
+                    with self.graph.device('/cpu:0'):
+                        summaries, eval_loss, _ = self.step(forward_only=True)
                     self.pipeline.toggle_active()
                     print("\tValidation loss = %.3f" % eval_loss, end="; ")
                     print("val perplexity = %.2f" % perplexity(eval_loss))

chatbot/recurrent_components.py
@@ -1,6 +1,6 @@
 import tensorflow as tf
 import pdb
-from utils.io_utils import EOS_ID, UNK_ID, GO_ID
+from utils.io_utils import EOS_ID, UNK_ID, GO_ID, PAD_ID
 from tensorflow.contrib.tensorboard.plugins import projector
 from tensorflow.contrib.training import bucket_by_sequence_length
 from tensorflow.contrib.rnn import GRUCell, LSTMCell, MultiRNNCell
@@ -123,8 +123,7 @@ class Decoder(RNN):
             self._projection = (w, b)
         super(Decoder, self).__init__(state_size, embed_size, dropout_prob, num_layers)
 
-    def __call__(self, inputs, initial_state=None, is_chatting=False,
-                 loop_embedder=None):
+    def __call__(self, inputs, initial_state=None, is_chatting=False, loop_embedder=None):
         """Run the inputs on the decoder. If we are chatting, then conduct dynamic sampling,
             which is the process of generating a response given inputs == GO_ID.
 
@@ -135,8 +134,6 @@ class Decoder(RNN):
                          returned Tensor shape.
             loop_embedder: required if is_chatting=False.
                            Embedder instance needed to feed decoder outputs as next inputs.
-            scope: (optional) variable scope name to use.
-
         Returns:
             outputs: if not is_chatting, tensor of shape [batch_size, max_time, output_size].
                      else, tensor of response IDs with shape [batch_size, max_time].
@@ -172,8 +169,8 @@ class Decoder(RNN):
 
             def cond(response, s):
                 """Input callable for tf.while_loop. See below."""
-                return tf.logical_and(tf.not_equal(response[-1], EOS_ID),
-                                      tf.less(tf.size(response), 100))
+                return tf.logical_or(tf.not_equal(response[-1], EOS_ID),
+                                     tf.not_equal(response[-1], PAD_ID))
 
             # Create integer (tensor) list of output ID responses.
             response = tf.stack([self.sample(outputs)])