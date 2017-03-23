0cb215d7f94fe6e32a263f664873e8ad79a07bbe
mckinziebrandon <mckinziebrandon@berkeley.edu>
2017-03-23 14:26:27 -0700

    New data classes for abstacting away the lower level aspects of preprocessing.

--- data/__init__.py
+++ data/__init__.py
@@ -1,3 +1,4 @@
+from data.data_helper import DataHelper
 from data._dataset import Dataset
 from data.dataset_wrappers import Cornell, Ubuntu, WMT, Reddit, TestData
 
--- data/data_helper.py
+++ data/data_helper.py
@@ -1,4 +1,4 @@
-"""Because I'm tired of thinking about paths and filenames."""
+"""For use in preprocessing stages. Because I'm tired of thinking about paths and filenames."""
 import os
 import logging
 import pandas as pd
@@ -6,7 +6,7 @@ import numpy as np
 import matplotlib.pyplot as plt
 import nltk
 from pprint import pprint
-from pympler import asizeof # for profiling memory usage
+from pympler.asizeof import asizeof # for profiling memory usage
 import enchant
 import json
 from itertools import chain
@@ -17,6 +17,9 @@ DATA_ROOTS = {'brandon': '/home/brandon/terabyte/Datasets/reddit',
               'ivan': '/Users/ivan/Documents/sp_17/reddit_data',
               'mitch': None}
 
+# smh
+HERE = os.path.dirname(os.path.realpath(__file__))
+
 
 class DataHelper:
     """Hi, I'm the DataHelper class. I manage file locations and how much computing resources
@@ -24,34 +27,45 @@ class DataHelper:
 
     def __init__(self, verbosity='info'):
 
-        if verbosity == 'info':
-            logging.basicConfig(level=logging.INFO)
-        else:
-            logging.basicConfig(level=logging.WARN)
+        if verbosity == 'info': logging.basicConfig(level=logging.INFO)
+        else: logging.basicConfig(level=logging.WARN)
 
         self.log = logging.getLogger('DataHelperLogger')
 
         # Figure out who we are talking to: ivan or brandon.
-        print("User name:")
+        print("User name:", end=" ")
         user = input().lower()
         if user not in ['brandon', 'ivan']: raise RuntimeError('Unknown User')
-        data_root = DATA_ROOTS[user]
-        print("Hello {}, I've set your data root as {}".format(user, data_root))
+        self.data_root = DATA_ROOTS[user]
+        print("Hello {}, I've set your data root as {}".format(user, self.data_root))
 
         # Which dataset we are working on.
-        print("Dataset name: [reddit, ubuntu, wmt, cornell]")
-        data_name = input().lower()
-        if data_name == 'reddit':
-            print("Year:")
-            year = input()
-            self.data_files = os.listdir(os.path.join(data_root, 'raw_data', year))
-            print("These are the files I found for that year:")
-            pprint(self.data_files)
+        #print("Dataset name: [reddit, ubuntu, wmt, cornell]")
+        #data_name = input().lower()
+        # TODO: generalize for any dataset, not just reddit.
+        #if data_name == 'reddit':
+        print("Year:", end=" ")
+        year = input()
+        data_files_rel_path = os.listdir(os.path.join(self.data_root, 'raw_data', year))
+        self.data_files = [os.path.join(self.data_root, 'raw_data', year, fname)
+                           for fname in data_files_rel_path]
+        print("These are the files I found for that year:")
+        pprint(self.data_files)
 
-        with open('dicts.json', 'r') as f:
+        with open(os.path.join(HERE, 'dicts.json'), 'r') as f:
             json_data = [json.loads(l) for l in f]
         # TODO: more descriptive names for the 'modify_' objects here would be nice.
-        modify_list, modify_value, contractions = json_data
+        self.modify_list, self.modify_value, self.contractions = json_data
+
+    def df_generator(self):
+        """Generates df from single files at a time."""
+        list_ = []
+        for i in range(len(self.data_files)):
+            df = pd.read_json(self.data_files[0], lines=True)
+            init_num_rows = len(df.index)
+            self.log.info("Number of lines in raw data file", init_num_rows)
+            self.log.info("Column names from raw data file: %r" % df.columns)
+            yield df
 
     def safe_load(self):
         """Load data while keeping an eye on memory usage."""
@@ -59,14 +73,42 @@ class DataHelper:
         # For in-place appending.
         # stackoverflow: import-multiple-csv-files-into-pandas-and-concatenate-into-one-dataframe
         list_ = []
-        for i in range(len(self.data_files)):
+        pbar = ProgressBar()
+        for i in pbar(range(len(self.data_files))):
             self.log.info("Starting to load file %s . . ." % self.data_files[i])
             # lines=True means "read as json-object-per-line."
             df = pd.read_json(self.data_files[0], lines=True)
             list_.append(df)
-        df = pd.concat(df)
+            self.log.info("Data list has size %.3f GiB" % (float(asizeof(list_)) / 1e9))
+        df = pd.concat(list_)
 
         init_num_rows = len(df.index)
         self.log.info("Number of lines in raw data file", init_num_rows)
         self.log.info("Column names from raw data file: %r" % df.columns)
         return df
+
+    def generate_files(self, from_file_path, to_file_path, conversation_ids, data):
+        """Generates two files, [from_file_path] and [to_file_path] of one-to-one comments
+        """
+        from_file_path = os.path.join(self.data_root, from_file_path)
+        to_file_path = os.path.join(self.data_root, to_file_path)
+
+        # Open the files and clear them.
+        from_file   = open(from_file_path, 'w')
+        to_file     = open(to_file_path, 'w')
+        from_file.write("")
+        to_file.write("")
+        from_file.close()
+        to_file.close()
+        for context, responses in conversation_ids:
+            from_file = open(from_file_path, 'a')
+            to_file = open(to_file_path, 'a')
+            # Since we have deleted comments, some comments parents might not exist anymore so we must catch that error.
+            for resp in responses:
+                try:
+                    from_file.write(data[context].replace('\n', '').replace('\r', ' ').replace('&gt', '') + "\n")
+                    to_file.write(data[resp].replace('\n', '').replace('\r', ' ').replace('&gt', '') + "\n")
+                except KeyError:
+                    pass
+        from_file.close()
+        to_file.close()
