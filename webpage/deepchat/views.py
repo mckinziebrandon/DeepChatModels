d4b4acc7742f58a923584f078e8111c1a00cc677
George <gkudrayvtsev@live.com>
2017-03-27 22:32:52 -0700

    Welcome to ; no more Django, added real chat i/o.

---
+++ webpage/deepchat/views.py
@@ -0,0 +1,16 @@
+from flask    import render_template
+from flask    import request
+
+from deepchat import app, csrf
+from .forms   import ChatForm
+
+@app.route('/', methods=[ 'GET' ])
+@app.route('/index', methods=[ 'GET' ])
+def index():
+    chat_form = ChatForm()
+    return render_template('index.html', form=chat_form)
+
+@app.route('/chat/', methods=[ 'POST' ])
+def chat():
+    chat_form = ChatForm()
+    return chat_form.message.data[::-1]
