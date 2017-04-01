f7e821b2d0bba716e5e26a0bb35307bba0b6201a
mckinziebrandon <mckinziebrandon@berkeley.edu>
2017-03-31 20:20:58 -0700

    Removed unused files.

--- tests/tf.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/bash
-
-# Amazing helper script to filter out all the annoying INFO log messages every time tensorflow starts. 
-# Author: github user Fenugreek. Source: https://github.com/tensorflow/tensorflow/issues/566
-python3 $* 3>&1 1>&2 2>&3 3>&- | grep -v ^I\ | grep -v ^pciBusID | grep -v ^major: | grep -v ^name: |grep -v ^Total\ memory:|grep -v ^Free\ memory:
