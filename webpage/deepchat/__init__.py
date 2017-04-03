8fbccb62bb22385ff7d76e266d32b8cfe9da9374
mckinziebrandon <mckinziebrandon@berkeley.edu>
2017-04-03 14:45:09 -0700

    Create bot in init.py and added tf requirement.

--- webpage/deepchat/__init__.py
+++ webpage/deepchat/__init__.py
@@ -1,8 +1,17 @@
 from flask     import Flask
 from flask_wtf import CSRFProtect
+from utils import io_utils
+
+# Don't judge me.
+from pydoc import locate
+from main import FLAGS
+FLAGS.pretrained_dir = "pretrained/reddit"
+config = io_utils.parse_config(FLAGS)
+dataset = locate(config['dataset'])(config['dataset_params'])
+bot = locate(config['model'])(dataset, config)
 
 app = Flask(__name__)
 app.config.from_object('config') # tells flask to read / use config.py
 csrf = CSRFProtect(app)
 
-from deepchat import views       # import at the end avoids circular imports
+from webpage.deepchat import views       # import at the end avoids circular imports
