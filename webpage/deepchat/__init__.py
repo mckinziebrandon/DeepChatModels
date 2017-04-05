d7a83ed4ee682706bb1dda31f2ce01c017b51319
Ivan Aguilar <aguilar@berkeley.edu>
2017-04-05 14:31:43 -0700

    modified init and requirements in webpage

--- webpage/deepchat/__init__.py
+++ webpage/deepchat/__init__.py
@@ -1,10 +1,6 @@
 from flask     import Flask
 from flask_wtf import CSRFProtect
 
-from utils import io_utils
-from utils import bot_freezer
-from pydoc import locate
-
 app = Flask(__name__)
 app.config.from_object('config') # tells flask to read / use config.py
 csrf = CSRFProtect(app)
