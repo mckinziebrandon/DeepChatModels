cb580e39ae743c468246d6bf810c9e9900f08ecc
mckinziebrandon <mckinziebrandon@berkeley.edu>
2017-04-08 11:31:56 -0700

    Tweaks applied for new web model.

--- chatbot/components/__init__.py
+++ chatbot/components/__init__.py
@@ -1,11 +1,12 @@
 from chatbot.components.embedder import Embedder
 from chatbot.components.input_pipeline import InputPipeline
-from chatbot.components.encoders import BasicEncoder, UniEncoder
+from chatbot.components.encoders import BasicEncoder, UniEncoder, BidirectionalEncoder
 from chatbot.components.decoders import BasicDecoder, AttentionDecoder
 
 __all__ = ["InputPipeline",
            "Embedder",
            "BasicEncoder",
+           "BidirectionalEncoder",
            "UniEncoder",
            "BasicDecoder",
            "AttentionDecoder"]
