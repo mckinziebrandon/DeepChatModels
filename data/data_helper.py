949c78f0b7973b9f86b2fa9897611a3755551373
George <george.k@berkeley.edu>
2017-04-10 23:47:45 -0700

    Adds minor refactoring / formatting changes.

--- data/data_helper.py
+++ data/data_helper.py
@@ -1,94 +1,119 @@
-"""For use in preprocessing stages. Because I'm tired of thinking about
-paths and filenames. Right now, is mainly for use by Brandon/Ivan. Will extend to
-general users in the future.
+""" Provides pre-processing functionality.
+
+Abstracts paths and filenames so we don't have to think about them. Currently,
+in use by Brandon / Ivan, but will extend to general users in the future.
 """
 import os
-import pdb
 import re
-import logging
-import pandas as pd
-import numpy as np
-from pprint import pprint
-from pympler.asizeof import asizeof # for profiling memory usage
+import pdb
 import json
-from progressbar import ProgressBar
-from subprocess import Popen, PIPE
+import logging
+from   pprint       import pprint
+from   subprocess   import Popen, PIPE
+
+import pandas as pd
+import numpy  as np
+from   pympler.asizeof  import asizeof          # for profiling memory usage
+from   progressbar      import ProgressBar
 
 # Absolute path to this file.
 _WORD_SPLIT = re.compile(r'([.,!?\"\':;)(])|\s')
 HERE = os.path.dirname(os.path.realpath(__file__))
-DATA_ROOTS = {'brandon': '/home/brandon/Datasets/reddit',
-        'ivan': '/Users/ivan/Documents/sp_17/reddit_data',
-        'mitch': '/Users/Mitchell/Documents/Chatbot/RedditData'}
-# Maximum memory usage allowed in GiB.
+DATA_ROOTS = {
+    'brandon': '/home/brandon/Datasets/reddit',
+    'ivan': '/Users/ivan/Documents/sp_17/reddit_data',
+    'mitch': '/Users/Mitchell/Documents/Chatbot/RedditData'
+    'george': '/Users/George/Documents/ChatbotData/reddit'
+}
+
+# Maximum memory usage allowed (in GiB).
 MAX_MEM = 2.0
 
+def prompt(text, default="", required=False):
+    print("%s (default=%r): " % (text, default), end="")
+    userinput = input()
+    while not userinput and required:
+        userinput = input("C'mon dude, be serious: ")
+
+    return userinput or default     # returns default if userinput is "truthy"!
 
 
 class DataHelper:
-    """Hi, I'm the DataHelper class. I manage file locations and how much computing resources
-    are being used in the preprocessing stages. I make it harder for you to screw up."""
+    """ Manages file locations and computing resource during preprocessing.
+
+    This interacts directly for the user and double-checks your work; I make it
+    harder for you to screw up. <-- Uh oh, I think it became self-aware.
+    """
 
     def __init__(self, log_level=logging.WARNING):
-        """Short convo with user that gives us what we need to help out."""
-
+        """ Establish some baseline data with the user.
+        """
         logging.basicConfig(filename='/tmp/data_helper.log', level=log_level)
-        # Keeps track of current file we're processing.
-        self.file_counter = 0
-        # Temporary work-around for parallel-processing with frequency dict.
-        self._word_freq = None
+
+        self.file_counter = 0   # current file we're processing
+        self._word_freq = None  # temporary: for parallelizing frequency dict
 
         print("Hi, I'm a DataHelper. For now, I support helping with the reddit dataset.")
-        print("For all questions, simply press ENTER if you want the default value.")
+        print("At any prompt, press ENTER if you want the default value.")
 
+        #
         # 1. Get user name. We can associate info with a given user as we go.
-        print("User name: (default=brandon)", end=" ")
-        user = input().lower()
-        if not user:
-            user = 'brandon'
+        #
+        user = prompt("Username", default="brandon").lower()
         if user not in DATA_ROOTS:
             print("I don't recognize you, %s." % user)
-            print("Please give me the path to your data:", end=" ")
-            self.data_root = input()
+            self.data_root = prompt("Please give me the path to your data:", required=True)
         else:
             self.data_root = DATA_ROOTS[user]
-        print("Hello {}, I've set your data root as {}".format(user, self.data_root))
 
+        print("Hello, %s, I've set your data root to %s" % (user, self.data_root))
+
+        #
         # 2. Get absolute paths to all data filenames in self.file_paths.
+        #
         self.file_paths = []
-        print("Years to process as comma-separated list: (default=2007,2008,2009)", end=" ")
-        years = input()
-        if not years: years = '2007,2008,2009'
-        years = years.split(',')
+        years = prompt("Years to process (as CSVs)", default="2007,2008,2009").split(',')
         for y in years:
+            # The path is: $ROOT/raw_data/$YEAR
+            # Add the entirety of the directory to the file paths.
             rel_paths = os.listdir(os.path.join(self.data_root, 'raw_data', y))
-            self.file_paths.extend(
-                    [os.path.join(self.data_root, 'raw_data', y, f) for f in rel_paths])
-            print("These are the files I found:")
+            self.file_paths.extend([
+                os.path.join(self.data_root, 'raw_data', y, f) for f in rel_paths
+            ])
+        print("These are the files I found:")
         pprint(self.file_paths)
         print()
 
-        print("Max memory to use? (default=%.2f GiB)" % MAX_MEM)
-        _max_mem = input()
-        if not _max_mem: self.max_mem = MAX_MEM
-        else: self.max_mem = float(_max_mem)
+        _max_mem = prompt("Maximum memory to use (in GiB)", "%.2f" % MAX_MEM)
+        try:
+            self.max_mem = float(_max_mem)
+        except ValueError:
+            print("C'mon dude, don't be a dildo.")
+            print("I haven't written this code well, so you'll have to start over.")
 
-        # Load the helper dictionaries from dicts.json.
-        with open(os.path.join(HERE, 'dicts.json'), 'r') as f:
-            json_data = [json.loads(l) for l in f]
+        # Load the helper dictionaries from dicts.json, which contain the following:
+        #
+        #   - First line:   a list of regular expressions to match against the data.
+        #   - Second line:  a list of _replacements_ for the mathces in line one.
+        #                   These should obviously have a 1-1 relationship.
+        #   - Third line:   k -> v pairs of contractions.
+        #
+        with open(os.path.join(HERE, "dicts.json"), 'r') as file:
+            json_data = [ json.loads(line) for line in file ]
+
             # TODO: more descriptive names for the 'modify_' objects here would be nice.
             self.modify_list, self.modify_value, self.contractions = json_data
 
     def safe_load(self):
-        """Load data while keeping an eye on memory usage."""
-
+        """ Load data while keeping an eye on memory usage.
+        """
         if self.file_counter >= len(self.file_paths):
             print("No more files to load!")
             return None
 
         # For in-place appending.
-        # S.O.: import-multiple-csv-files-into-pandas-and-concatenate-into-one-dataframe
-        list_ = []
+        # S.O.: https://stackoverflow.com/questions/20906474/
+        list_ = []  # real descriptive :)
         pbar = ProgressBar()
         for i in pbar(range(self.file_counter, len(self.file_paths))):
             # lines=True means "read as json-object-per-line."
@@ -97,14 +122,15 @@ class DataHelper:
             mem_usage = float(asizeof(list_)) / 1e9
             logging.info("Data list has size %.3f GiB" % mem_usage)
             if mem_usage > self.max_mem:
-                print("Past max capacity: %r. Leaving data collection early." % mem_usage)
+                print("Past max capacity: %r! Leaving data collection early." % mem_usage)
                 break
-        self.file_counter = i + 1
+
+        self.file_counter = i + 1   # excuse me what
 
         df = pd.concat(list_).reset_index()
         init_num_rows = len(df.index)
-        logging.info("Number of lines in raw data file", init_num_rows)
-        logging.info("Column names from raw data file: %r" % df.columns)
+        logging.info("Number of lines in raw data file: %r" % init_num_rows)
+        logging.info("Column names from raw data file: %r"  % df.columns)
         logging.info("DataHelper.safe_load: df.head() = %r" % df.head())
         return df
 
@@ -116,7 +142,7 @@ class DataHelper:
         return self._word_freq
 
     def generate_files(self, from_file_path, to_file_path, root_to_children, comments_dict):
-        """Generates two files, [from_file_path] and [to_file_path] of one-to-one comments
+        """ Generates two files, [from_file_path] and [to_file_path] of 1-1 comments.
         """
         from_file_path = os.path.join(self.data_root, '2010',from_file_path)
         to_file_path = os.path.join(self.data_root, '2010', to_file_path)
@@ -131,29 +157,32 @@ class DataHelper:
                         except KeyError:
                             pass
 
-        (num_samples, stderr) = Popen(['wc', '-l', from_file_path], stdout=PIPE).communicate()
+        (num_samples, stderr) = Popen([ 'wc', '-l', from_file_path ], stdout=PIPE).communicate()
         num_samples = int(num_samples.strip().split()[0])
+
         print("Final processed file has %d samples total." % num_samples)
+
         # First make sure user has copy of bash script we're about to use.
         os.popen('cp %s %s' % (os.path.join(HERE, 'split_into_n.sh'), self.data_root))
+
         # Split data into 90% training and 10% validation.
         os.popen('bash %s %d' % (os.path.join(self.data_root, 'split_into_n.sh'),
-                            0.1 * num_samples))
-
-
+                                 0.1 * num_samples))
 
     def df_generator(self):
-        """Generates df from single files at a time."""
+        """ Generates df from single files at a time.
+        """
         for i in range(len(self.file_paths)):
             df = pd.read_json(self.file_paths[i], lines=True)
             init_num_rows = len(df.index)
-            logging.info("Number of lines in raw data file", init_num_rows)
-            logging.info("Column names from raw data file: %r" % df.columns)
+            logging.info("Number of lines in raw data file: %r" % init_num_rows)
+            logging.info("Column names from raw data file: %r"  % df.columns)
             yield df
 
     @staticmethod
     def random_rows_generator(num_rows_per_print, num_rows_total):
-        """Fun generator for viewing random comments (rows) in dataframes."""
+        """ Fun generator for viewing random comments (rows) in dataframes.
+        """
         num_iterations = num_rows_total // num_rows_per_print
         shuffled_indices = np.arange(num_rows_per_print * num_iterations)
         np.random.shuffle(shuffled_indices)
@@ -162,11 +191,15 @@ class DataHelper:
 
     @staticmethod
     def word_tokenizer(sentences):
-        """Tokenizes sentence/list of sentences into word tokens."""
+        """ Tokenizes sentence / list of sentences into word tokens.
+        """
 
-        # Suggestion by user: Shaptic.
-        tokenized = [None for _ in range(len(sentences))]
+        # Minor optimization: pre-create the list and fill it.
+        tokenized = [ None for _ in range(len(sentences)) ]
         for i in range(len(sentences)):
-            tokenized[i] = [w for w in _WORD_SPLIT.split(sentences[i].strip()) if w]
+            tokenized[i] = [
+                w for w in _WORD_SPLIT.split(sentences[i].strip()) if w
+            ]
+
         return tokenized
 
